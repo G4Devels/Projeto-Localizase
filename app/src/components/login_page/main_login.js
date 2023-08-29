568476b9b5f800d4f7d2aeb896836042b6998522
Nícolas Carvalho <nicolassprofissional@gmail.com>
2023-08-29 07:13:54 -0300

    getting user json data from login

--- app/src/components/login_page/main_login.js
+++ app/src/components/login_page/main_login.js
@@ -1,17 +1,29 @@
 import '../../component_styles/login_page.css'
 
 export default function MainLogin () {
+
+    function getData(eventObj) {
+        eventObj.preventDefault()
+
+        const data = {
+            email: eventObj.target.elements.email.value,
+            password: eventObj.target.elements.password.value
+        }
+
+        console.log(data)
+    }
+
     return (
         <>
 
-            <div id="login">
+            <div id="login" onSubmit={getData}>
                 <img src={require('../../assets/localizase_logo.png')} alt="Logo do localizase"/>
 
                 <form>
                     <h1>Login</h1>
 
-                    <input type="email" placeholder="Digite seu e-mail" required></input>
-                    <input type="password" placeholder="Digite sua senha" required></input>
+                    <input name="email" type="email" placeholder="Digite seu e-mail" required></input>
+                    <input name="password" type="password" placeholder="Digite sua senha" required></input>
 
                     <button type="submit">Entrar</button>
                 </form>
