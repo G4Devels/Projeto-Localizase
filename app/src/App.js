1ae6acf3d88bbabedc24f5d67123653b5c8d32cc
Daniel-Anastacio <daniel.anastacio556@gmail.com>
2023-09-08 11:09:03 -0300

    [FIX]Consertando App.js que estava mandando para o registro

--- app/src/App.js
+++ app/src/App.js
@@ -1,21 +1,10 @@
 import MainLogin from './components/login_page/main_login'
-import MainRegistration from './components/registration_page/main_registration';
 import { EmailAndPasswordProvider } from './contexts/authEmailAndPasswordContext';
-
-//Alterei para testar a tela de registro, mais tarde farei as rotas
-
-// function App() {
-//   return (
-//     <EmailAndPasswordProvider>
-//       <MainLogin/>
-//     </EmailAndPasswordProvider>
-//   );
-// }
 
 function App() {
   return (
     <EmailAndPasswordProvider>
-      <MainRegistration/>
+      <MainLogin/>
     </EmailAndPasswordProvider>
   );
 }
