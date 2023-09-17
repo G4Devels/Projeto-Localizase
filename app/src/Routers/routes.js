c9496f56ba70428f1fb17af1f88b0a0a8b7cbf63
Camilay3 <camila.santiago026@gmail.com>
2023-09-16 21:40:38 -0300

    header

--- app/src/Routers/routes.js
+++ app/src/Routers/routes.js
@@ -3,12 +3,14 @@ import { PrivateRoutes } from ".";
 import { MainHome } from "../components/home_page/main_home";
 import { MainLogin } from "../components/login_page/main_login";
 import MainRegistration from "../components/registration_page/main_registration";
+import { MainLanding } from "../components/landing_page/main_landing";
 
 
 export const AppRoutes = () => {
     return (
     <BrowserRouter>
         <Routes>
+            <Route path="/landing" element={<MainLanding />} />
             <Route path="/" element={<MainLogin />} />
             <Route path="/registro" element={<MainRegistration />}/>
             <Route path="/home" element={<PrivateRoutes />}>
