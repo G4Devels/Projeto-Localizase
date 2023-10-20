909a4efea04497edabf2b9f2b3a3fa7a6389ec84
Nícolas Carvalho <nicolassprofissional@gmail.com>
2023-10-20 10:22:39 -0300

    adding firebaseConfig

---
+++ server-express/services/firebaseConfig.js
@@ -0,0 +1,16 @@
+const { initializeApp } = require('firebase-admin/app');
+const { getFirestore } = require('firebase-admin/firestore');
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCh574cpIggCGFKCz2MUeeCfp4XnlUhoJY",
+    authDomain: "localizase-b0c83.firebaseapp.com",
+    databaseURL: "https://localizase-b0c83-default-rtdb.firebaseio.com",
+    projectId: "localizase-b0c83",
+    storageBucket: "localizase-b0c83.appspot.com",
+    messagingSenderId: "71684198008",
+    appId: "1:71684198008:web:09ba1bd580f4d77ce7278d",
+    measurementId: "G-Q2XWX8T992"
+  };  
+
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
