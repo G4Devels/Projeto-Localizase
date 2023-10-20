67a0a6d7249a350f22578ed0b2ebff85bef625b8
Nícolas Carvalho <nicolassprofissional@gmail.com>
2023-10-20 12:59:46 -0300

    modifying method for test

--- server-express/routes/getRecomendados.js
+++ server-express/routes/getRecomendados.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.post('/recomendados', function(req, res, next) {
+//const { db } = require('../services/firebaseFirestoreConfig');
+
+// POST getRecomendados page.
+router.get('/recomendados/', function(req, res, next) {
+
+  // const userID = req.params.userID
+  // console.log(userID)
   console.log('Pedido recebido [RECOMENDADOS]')
+  res.send('<h1>teste</h1>')
+
 });
 
 module.exports = router;
