eff1d77a510bebbf1b9da963151154fc9c83a92c
Nícolas Carvalho <nicolassprofissional@gmail.com>
2023-10-21 18:36:18 -0300

    finishing getrecomendados route

--- server-express/app.js
+++ server-express/app.js
@@ -11,12 +11,15 @@ const serviceAccount = require('./API_keys/localizase-b0c83-591dc4e68d1c.json');
 initializeApp({
     credential: cert(serviceAccount)
 })
+
 const db = getFirestore()
 
 
 
 app.get('/getrecomendados/:userID', async (req, res)=>{
+
     console.log('[getRecomendados] ON')
+
 
     // getting user tags
     const userID = req.params.userID
@@ -24,18 +27,59 @@ app.get('/getrecomendados/:userID', async (req, res)=>{
     const userDocRef = db.collection('users').doc(userID);
     const userDocData = await userDocRef.get()
 
-    let userTagsIds = []
+    let userTagsIDs = []
 
     if (!userDocData.exists) {
         res.send('No such document!')
     } else {
-        userTagsIds = userDocData.data().tags
+        userTagsIDs = userDocData.data().tags
     }
+
+
+    // calculating points of locations
+    const locationsRef = db.collection('locations');
+    let dataAndPoints = []
+
+    const snapshot = await locationsRef.get();
+    if (!snapshot.empty) {
+        snapshot.forEach(doc => {
+            const tagsLocationIDs = doc.data().tags.map(tagRef => tagRef._path.segments).map(tagID => tagID[1]);
+
+            let tagPoints = tagsLocationIDs.map(locationTagID => {
+                if (locationTagID in userTagsIDs) {
+                    return 1
+                }
+            })
+
+            dataAndPoints.push({
+                tagPoints: tagPoints.length,
+                data: doc.data()
+            })
+
+        });
+    }
+    
+
+    // ordering by tag quantity/points
+    dataAndPoints.sort((a, b) => {
+        return b.tagPoints - a.tagPoints
+    })
+
+
+    // getting locations data only
+    const locationsDataOnly = dataAndPoints.map(location => location.data)
+
+
+    // sedding ordered locations data
+    res.send(locationsDataOnly)
+
 })
+
 
 app.get('getemalta', (req, res)=>{
     console.log('[getEmAlta] ON')
 })
+
 
 app.listen(port, ()=>{
     console.log('[SERVER] OK')
