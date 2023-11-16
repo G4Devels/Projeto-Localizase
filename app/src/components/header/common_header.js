70d15799b85634d32fd330254892cd1ec6645333
Nícolas Carvalho <nicolassprofissional@gmail.com>
2023-11-16 13:32:59 -0300

    adjusting font sizes and responsiveness

--- app/src/components/header/common_header.js
+++ app/src/components/header/common_header.js
@@ -1,22 +1,48 @@
 import { Link } from 'react-router-dom';
 import { AuthAccountsContext } from '../../contexts/authAccounts';
-import { useContext } from 'react';
+import { useContext, useRef } from 'react';
+import {FaBars, FaTimes} from 'react-icons/fa'
 
 import '../../component_styles/common_header.css';
 
 export const CommonHeader = () => {
 
     const { signed } = useContext(AuthAccountsContext);
+    const navRef = useRef();
+
+    const showNavBar = () => {
+        navRef.current.classList.toggle('responsive-nav')
+    }
 
     return (
         <>
         
             <header className='common-header'>
-                    <nav className="header-nav">
-                        <img src={require('../../assets/localizase_logo.png')} alt="Logo do localizase"/>
-                        
-                        <Link className="a" to={signed ? "/home" : "/login"}>Entrar</Link>
-                    </nav>
+
+                    <div className='common-header-content'>
+
+                        <img className='localizase-logo' src={require('../../assets/localizase_logo.png')} alt="Logo do localizase"/>
+                            
+                        <nav ref={navRef} className="header-nav">
+
+                            <a className='nav-link-to-section' href='#about-us'>Sobre nós</a>
+                            <a className='nav-link-to-section' href='#our-service'>Nosso serviço</a>
+                            <a className='nav-link-to-section' href='#experience'>Experiências</a>
+                            
+                            <Link className="link-btn" to={signed ? "/home" : "/login"}>Entrar</Link>
+                            
+                            <button className='nav-btn nav-btn-close' onClick={showNavBar}>
+                                <FaTimes/>
+                            </button>
+
+                        </nav>
+
+                        <button className='nav-btn nav-btn-open' onClick={showNavBar}>
+                            <FaBars/>
+                        </button>
+
+                    </div>
+
             </header>    
         
         </>
